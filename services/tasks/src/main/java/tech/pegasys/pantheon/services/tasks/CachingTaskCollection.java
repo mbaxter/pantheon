9016679d70538d28a3b0c4bed54a53c62e4b3c05
Meredith Baxter <mbaxter@users.noreply.github.com>
2019-03-12 11:04:44 -0400

    Fix comment

--- services/tasks/src/main/java/tech/pegasys/pantheon/services/tasks/CachingTaskCollection.java
+++ services/tasks/src/main/java/tech/pegasys/pantheon/services/tasks/CachingTaskCollection.java
@@ -24,7 +24,9 @@ public class CachingTaskCollection<T> implements TaskCollection<T> {
 
   // The underlying collection
   private final TaskCollection<T> wrappedCollection;
-  // A cache of tasks to operate on before going to taskCollection
+  /**
+   * A cache of tasks to operate on before going to {@link CachingTaskCollection#wrappedCollection}
+   */
   private final Queue<Task<T>> cache = new ArrayDeque<>();
   // Tasks that have been removed, but not marked completed yet
   private final Set<Task<T>> outstandingTasks = new HashSet<>();
