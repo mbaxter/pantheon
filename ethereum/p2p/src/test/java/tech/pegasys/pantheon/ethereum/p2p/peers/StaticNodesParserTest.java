2fedbcfd4312fb303d5060bde35af699f89e644b
Meredith Baxter <mbaxter@users.noreply.github.com>
2019-04-12 18:10:09 -0400

    Fix test - sets do not have a guaranteed ordering

--- ethereum/p2p/src/test/java/tech/pegasys/pantheon/ethereum/p2p/peers/StaticNodesParserTest.java
+++ ethereum/p2p/src/test/java/tech/pegasys/pantheon/ethereum/p2p/peers/StaticNodesParserTest.java
@@ -67,7 +67,7 @@ public class StaticNodesParserTest {
     final File validFile = new File(resource.getFile());
     final Set<EnodeURL> enodes = StaticNodesParser.fromPath(validFile.toPath());
 
-    assertThat(enodes).containsExactly(validFileItems.toArray(new EnodeURL[validFileItems.size()]));
+    assertThat(enodes).containsExactlyInAnyOrder(validFileItems.toArray(new EnodeURL[validFileItems.size()]));
   }
 
   @Test
