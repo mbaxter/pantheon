650c05b4e919782cdeee99873124e9f746b901a1
Meredith Baxter <mbaxter@users.noreply.github.com>
2019-05-14 18:00:38 -0400

    Fix PantheonCommandTest by using valid node id

--- pantheon/src/test/java/tech/pegasys/pantheon/cli/PantheonCommandTest.java
+++ pantheon/src/test/java/tech/pegasys/pantheon/cli/PantheonCommandTest.java
@@ -882,7 +882,9 @@ public class PantheonCommandTest extends CommandTestAbstract {
 
   @Test
   public void p2pOptionsRequiresServiceToBeEnabled() {
-    final String[] nodes = {"0001", "0002", "0003"};
+    final String[] nodes = {
+      "6f8a80d14311c39f35f516fa664deaaaa13e85b2f7493f37f6144d86991ec012937307647bd3b9a82abe2974e1407241d54947bbb39763a4cac9f77166ad92a0"
+    };
 
     parseCommand(
         "--p2p-enabled",
