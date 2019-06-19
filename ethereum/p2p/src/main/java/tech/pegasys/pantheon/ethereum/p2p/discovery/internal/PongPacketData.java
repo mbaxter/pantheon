909a45ebf7262bd047e207693bcd81c265671f9a
Meredith Baxter <mbaxter@users.noreply.github.com>
2019-06-19 14:48:15 -0400

    Be more lenient with discovery message deserialization

--- ethereum/p2p/src/main/java/tech/pegasys/pantheon/ethereum/p2p/discovery/internal/PongPacketData.java
+++ ethereum/p2p/src/main/java/tech/pegasys/pantheon/ethereum/p2p/discovery/internal/PongPacketData.java
@@ -44,7 +44,7 @@ public class PongPacketData implements PacketData {
     final Endpoint to = Endpoint.decodeStandalone(in);
     final BytesValue hash = in.readBytesValue();
     final long expiration = in.readLongScalar();
-    in.leaveList();
+    in.leaveList(true);
     return new PongPacketData(to, hash, expiration);
   }
 
